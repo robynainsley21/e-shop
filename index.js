be1ec4d62dae019eee276ac071da0154359e1f18
robynainsley21 <robynainsley2i@gmail.com>
2024-08-12 16:09:37 +0200

    Delete and Login

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import path from "path";
 import { connection as db } from "./config/index.js";
 import { createToken } from "./middleware/AuthenticateUser.js";
-import { hash } from "bcrypt";
+import { hash, compare } from "bcrypt";
 import bodyParser from "body-parser";
 
 /**Creating the express app */
@@ -40,7 +40,7 @@ router.get("^/$|/eShop", (req, res) => {
 router.get("/users", (req, res) => {
   try {
     const strQry = `
-        SELECT firstName, lastName, age, emailAdd
+        SELECT firstName, lastName, age, emailAdd, pwd
         FROM Users;
         `;
     db.query(strQry, (err, results) => {
@@ -149,6 +149,78 @@ router.patch("/user/:id", async (req, res) => {
     });
   } catch (error) {
     res.json({ status: 404, message: error.message });
+  }
+});
+
+/**
+ * Deleting a specific user
+ */
+router.delete("/user/:id", (req, res) => {
+  try {
+    /**
+     * we're using the WHERE clause because we are deleting only one user
+     * without it, we would delete all users
+     */
+    const strQry = `
+      DELETE FROM Users
+      WHERE userID = ${req.params.id};
+    `;
+    db.query(strQry, (err) => {
+      if (err)
+        throw new Error(`To delete a user, please review your delete query.`);
+      res.json({
+        status: res.statusCode,
+        message: "User's account has been deleted.",
+      });
+    });
+  } catch (error) {
+    res.json({ status: 404, message: error.message });
+  }
+});
+
+/**
+ * Login
+ */
+router.post("/login", (req, res) => {
+  try {
+    const { emailAdd, pwd } = req.body;
+    const strQry = `
+      SELECT userID, firstName, lastName, age, emailAdd, pwd
+      FROM Users
+      WHERE emailAdd = '${emailAdd}'
+      ;
+    `;
+    db.query(strQry, async (err, result) => {
+      if (err) throw new Error(`To login, please review your query.`);
+      if (!result?.length) {
+        res.json({
+          status: 401,
+          message: "You provided the wrong email.",
+        });
+      } else {
+        //first argument is the user's password, the second is the password from the database
+        const isValidPass = await compare(pwd, result[0].pwd);
+        if (isValidPass) {
+          const token = createToken({ emailAdd, pwd });
+          res.json({
+            status: res.statusCode,
+            token,
+            //returning the first in the array returns it as a single object instead of the entire array
+            result: result[0],
+          });
+        } else {
+          res.json({
+            status: 401,
+            message: `Invalid password or you have not registered.`,
+          });
+        }
+      }
+    });
+  } catch (error) {
+    res.json({
+      status: 401,
+      message: error.message,
+    });
   }
 });
 
